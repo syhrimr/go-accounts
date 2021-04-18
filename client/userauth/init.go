66b2c511b45ec29b8b488ffe5a75f4a324f6d84f
Syahri Maulana Ramadhan <syahri.maulana@gmail.com>
2021-04-18 12:57:50 +0800

    client auth

---
+++ client/userauth/init.go
@@ -0,0 +1,28 @@
+package userauth
+
+import (
+	"time"
+)
+
+type AuthClient struct {
+	host    string
+	timeout time.Duration
+}
+
+type User struct {
+	UserID     int64     `json:"user_id"`
+	Username   string    `json:"username"`
+	CreatedAt  time.Time `json:"created_at"`
+	ProfilePic string    `json:"profile_pic"`
+}
+
+type ClientItf interface {
+	GetUserInfo(accessToken string) *User
+}
+
+func NewClient(host string, timeout time.Duration) ClientItf {
+	return &AuthClient{
+		host:    host,
+		timeout: timeout,
+	}
+}
